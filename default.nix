e257cf916d22bd5f03e67ca91ff95ce12a0145d6
kelton o'brien <kelton@keltono.net>
2021-08-02 22:32:39 -0500

    refactor out nix

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs.python3Packages;
-
-buildPythonApplication {
-  pname = "app";
-  src = ./src;
-  version = "0.2";
-  propagatedBuildInputs = [ flask werkzeug ];
-}
